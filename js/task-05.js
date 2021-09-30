fec84ffef0daa52798d21cfdfcdf0dc1c0b13fd4
Malogon Anastasua <malogonanastasia@gmail.com>
2021-09-30 11:21:44 +0300

    update styles

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,9 @@
 const textInput = document.querySelector(`#name-input`);
-const output = document.querySelector(`#name-output`);
+const textOutput = document.querySelector(`#name-output`);
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
-  if (output.textContent === "") {
-    output.textContent = "Anonymous";
+  textOutput.textContent = event.currentTarget.value;
+  if (textOutput.textContent === "") {
+    textOutput.textContent = "Anonymous";
   }
 });
