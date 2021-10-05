7cb2e490f3fb2d526997ee7c236275c676b630b3
Malogon Anastasua <malogonanastasia@gmail.com>
2021-10-05 14:37:35 +0300

    update task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector("body");
+
+const bodyRef = document.querySelector("body");
+const buttonRef = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+
+buttonRef.addEventListener("click", changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = color;
+  colorRef.textContent = color;
+}
