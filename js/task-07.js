394fe09f656e0441d007871dbb4128fb66022474
Malogon Anastasua <malogonanastasia@gmail.com>
2021-10-05 15:20:15 +0300

    update task8

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector("#font-size-control");
 const spanRef = document.querySelector("#text");
 
 function changeFontSize(event) {
-    spanRef.style.fontSize = `${event.currentTarget.value}px`;
+    spanRef.style.fontSize = `${event.target.value}px`;
 }
 
 inputRef.addEventListener("input", changeFontSize);
